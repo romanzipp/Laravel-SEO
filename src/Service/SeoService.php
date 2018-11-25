c32df5888d2b225d905ed7dd3d3db26ea0f9cf7b
Roman Zipp <ich@ich.wtf>
2018-11-25 15:15:23 +0100

    Add service base

---
+++ src/Service/SeoService.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace romanzipp\Seo\Service;
+
+use romanzipp\Seo\Service\Traits\RenderTrait;
+use romanzipp\Seo\Service\Traits\SetterTrait;
+
+class SeoService
+{
+    use RenderTrait;
+    use SetterTrait;
+}
---
+++ src/Service/Tratis/RenderTrait.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace romanzipp\Seo\Service\Traits;
+
+trait RenderTrait
+{
+}
