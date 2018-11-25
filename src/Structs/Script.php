a8d145cbf1593d061e54bf301986579a7bcf35ee
Roman Zipp <ich@ich.wtf>
2018-11-25 22:11:56 +0100

    Add root structs references

--- src/Structs/Script.php
+++ src/Structs/Script.php
@@ -4,6 +4,9 @@ namespace romanzipp\Seo\Structs;
 
 use romanzipp\Seo\Structs\Struct;
 
+/**
+ * @see https://github.com/joshbuchea/HEAD#elements
+ */
 class Script extends Struct
 {
     protected $unique = false;
--- src/Structs/Title.php
+++ src/Structs/Title.php
@@ -4,6 +4,9 @@ namespace romanzipp\Seo\Structs;
 
 use romanzipp\Seo\Structs\Struct;
 
+/**
+ * @see https://github.com/joshbuchea/HEAD#elements
+ */
 class Title extends Struct
 {
     protected function tag(): string
