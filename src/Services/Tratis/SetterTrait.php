a10d71f36e3593115bc15f9c754a5db7bda7b64b
Roman Zipp <ich@ich.wtf>
2018-11-25 17:56:38 +0100

    Populate service setter trait

--- src/Services/Tratis/SetterTrait.php
+++ src/Services/Tratis/SetterTrait.php
@@ -2,6 +2,51 @@
 
 namespace romanzipp\Seo\Services\Traits;
 
+use romanzipp\Seo\Structs\Presets\OpenGraph;
+use romanzipp\Seo\Structs\Presets\Twitter;
+use romanzipp\Seo\Structs\Struct;
+
 trait SetterTrait
 {
+    abstract public function getStructs(): array;
+
+    /**
+     * Add struct.
+     *
+     * @param Struct $struct
+     */
+    public function add(Struct $struct): self
+    {
+        $this->structs[] = $struct;
+
+        return $this;
+    }
+
+    /**
+     * Add Twitter struct.
+     *
+     * @param  string     $name
+     * @param  mixed|null $content
+     * @return self
+     */
+    public function twitter(string $name, $content = null): self
+    {
+        return $this->add(
+            Twitter::make()->name($name)->content($content)
+        );
+    }
+
+    /**
+     * Add OpenGraph struct.
+     *
+     * @param  string     $property
+     * @param  mixed|null $content
+     * @return self
+     */
+    public function og(string $property, $content = null): self
+    {
+        return $this->add(
+            OpenGraph::make()->property($property)->content($content)
+        );
+    }
 }
