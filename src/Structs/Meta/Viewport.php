566d455c92a38875058c2586acaed67aafb3026c
Roman Zipp <ich@ich.wtf>
2018-11-25 22:23:43 +0100

    Add viewport struct

---
+++ src/Structs/Meta/Viewport.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace romanzipp\Seo\Structs\Meta;
+
+use romanzipp\Seo\Structs\Meta;
+
+/**
+ * @see https://github.com/joshbuchea/HEAD#recommended-minimum
+ */
+class Viewport extends Meta
+{
+    public static function defaults($struct)
+    {
+        $struct->attr('name', 'viewport');
+    }
+
+    public function content(string $content): self
+    {
+        $this->addAttribute('content', $content);
+
+        return $this;
+    }
+}
