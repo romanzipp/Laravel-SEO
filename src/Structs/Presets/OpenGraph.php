21fece3b368386df83111391a8f9cac97325c122
Roman Zipp <ich@ich.wtf>
2018-11-25 17:33:01 +0100

    Fix og & twitter struct presets

--- src/Structs/Presets/OpenGraph.php
+++ src/Structs/Presets/OpenGraph.php
@@ -4,6 +4,19 @@ namespace romanzipp\Seo\Structs\Presets;
 
 use romanzipp\Seo\Structs\Meta;
 
-class MetaOpenGraph extends Meta
+class OpenGraph extends Meta
 {
+    public function property(string $value): self
+    {
+        $this->addAttribute('property', 'og:' . $value);
+
+        return $this;
+    }
+
+    public function content(string $value): self
+    {
+        $this->addAttribute('content', $value);
+
+        return $this;
+    }
 }
--- src/Structs/Presets/Twitter.php
+++ src/Structs/Presets/Twitter.php
@@ -4,6 +4,19 @@ namespace romanzipp\Seo\Structs\Presets;
 
 use romanzipp\Seo\Structs\Meta;
 
-class MetaOpenGraph extends Meta
+class Twitter extends Meta
 {
+    public function name(string $value): self
+    {
+        $this->addAttribute('name', 'twitter:' . $value);
+
+        return $this;
+    }
+
+    public function content(string $value): self
+    {
+        $this->addAttribute('content', $value);
+
+        return $this;
+    }
 }
