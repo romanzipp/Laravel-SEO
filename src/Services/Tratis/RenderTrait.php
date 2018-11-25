85c6201b9cc620fb9a6a416f4ad36f92e70ba0ed
Roman Zipp <ich@ich.wtf>
2018-11-25 17:40:57 +0100

    Add service render method

--- src/Services/Tratis/RenderTrait.php
+++ src/Services/Tratis/RenderTrait.php
@@ -2,6 +2,24 @@
 
 namespace romanzipp\Seo\Services\Traits;
 
+use Illuminate\Support\HtmlString;
+use romanzipp\Seo\Builders\StructBuilder;
+
 trait RenderTrait
 {
+    /**
+     * Render all applied structs.
+     *
+     * @return HtmlString
+     */
+    public function render(): HtmlString
+    {
+        $structs = $this->structs;
+
+        $contents = array_map(function ($struct) {
+            return StructBuilder::build($struct)->toHtml();
+        }, $structs);
+
+        return new HtmlString(implode(PHP_EOL, $contents));
+    }
 }
