1e70fa7af19e971eb347e9ac9b5935d326172c4d
Roman Zipp <ich@ich.wtf>
2018-11-26 17:37:41 +0100

    Populate hooks test

--- tests/HooksTest.php
+++ tests/HooksTest.php
@@ -4,19 +4,39 @@ namespace romanzipp\Seo\Test;
 
 use romanzipp\Seo\Facades\Seo;
 use romanzipp\Seo\Helpers\Hook;
-use romanzipp\Seo\Services\SeoService;
 use romanzipp\Seo\Structs\Meta\OpenGraph;
 use romanzipp\Seo\Structs\Title;
 use romanzipp\Seo\Test\TestCase;
 
 class HooksTest extends TestCase
 {
-    public function testInstance()
+    public function testBodyHooks()
     {
-        $seo = app(SeoService::class);
-
         Title::hook(
-            (new Hook)
+            Hook::make()
+                ->onBody()
+                ->callback(function ($body) {
+                    return $body . ' 1';
+                })
+        );
+
+        seo()->add(
+            Title::make()->body('My Title')
+        );
+
+        $contents = seo()->renderContentsArray();
+
+        $this->assertCount(1, $contents);
+
+        $this->assertEquals('<title>My Title 1</title>', $contents[0]);
+
+        Title::clearHooks();
+    }
+
+    public function testMultipleBodyHooks()
+    {
+        Title::hook(
+            Hook::make()
                 ->onBody()
                 ->callback(function ($body) {
                     return $body . ' 1';
@@ -27,17 +47,40 @@ class HooksTest extends TestCase
             Hook::make()
                 ->onBody()
                 ->callback(function ($body) {
-                    return $body . ' 1';
+                    return $body . ' 2';
                 })
         );
 
+        seo()->add(
+            Title::make()->body('My Title')
+        );
+
+        $contents = seo()->renderContentsArray();
+
+        $this->assertEquals('<title>My Title 1 2</title>', $contents[0]);
+
+        Title::clearHooks();
+    }
+
+    public function testAttributesHooks()
+    {
         OpenGraph::hook(
             Hook::make()
                 ->onAttributes()
                 ->whereAttribute('property', 'og:title')
                 ->callback(function ($attributes) {
-                    return $attributes;
+                    return array_merge(['should' => 'exist'], $attributes);
                 })
         );
+
+        seo()->add(
+            OpenGraph::make()->property('title')->content('title')
+        );
+
+        $contents = seo()->renderContentsArray();
+
+        $this->assertRegexp('/should\=\"exist\"/', $contents[0]);
+
+        OpenGraph::clearHooks();
     }
 }
