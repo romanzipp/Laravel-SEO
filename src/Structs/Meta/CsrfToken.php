35a92d086ecfa5217b417d27f3170b05fd67c7fb
Roman Zipp <ich@ich.wtf>
2018-11-26 23:02:11 +0100

    Add csrf token struct

---
+++ src/Structs/Meta/CsrfToken.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace romanzipp\Seo\Structs\Meta;
+
+use romanzipp\Seo\Structs\Meta;
+use romanzipp\Seo\Structs\Struct;
+
+class CsrfToken extends Meta
+{
+    protected $unique = true;
+
+    public static function defaults(Struct $struct)
+    {
+        $struct->attr('name', 'csrf-token');
+    }
+
+    public function token(string $token): self
+    {
+        $this->addAttribute('content', $token);
+
+        return $this;
+    }
+}
