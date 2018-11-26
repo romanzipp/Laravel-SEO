2ea88ba8859ce031d20fc497a32a92b9f13c9dc0
Roman Zipp <ich@ich.wtf>
2018-11-26 17:18:29 +0100

    Add instance tests

--- tests/InstantiationTest.php
+++ tests/InstantiationTest.php
@@ -3,15 +3,29 @@
 namespace romanzipp\Seo\Test;
 
 use romanzipp\Seo\Facades\Seo;
+use romanzipp\Seo\Helpers\Hook;
 use romanzipp\Seo\Services\SeoService;
+use romanzipp\Seo\Structs\Meta;
 use romanzipp\Seo\Test\TestCase;
 
 class InstantiationTest extends TestCase
 {
-    public function testInstance()
+    public function testServiceInstance()
     {
+        $this->assertInstanceOf(SeoService::class, app(SeoService::class));
+
         $this->assertInstanceOf(SeoService::class, Seo::make());
 
         $this->assertInstanceOf(SeoService::class, seo());
+    }
+
+    public function testHookInstance()
+    {
+        $this->assertInstanceOf(Hook::class, Hook::make());
+    }
+
+    public function testStructInstance()
+    {
+        $this->assertInstanceOf(Meta::class, Meta::make());
     }
 }
