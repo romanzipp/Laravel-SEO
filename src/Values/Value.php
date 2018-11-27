cd58705d53667c2253aa4d388c529e9329ab6700
Roman Zipp <ich@ich.wtf>
2018-11-27 14:58:19 +0100

    Add value class + body & attribute class

---
+++ src/Values/Value.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace romanzipp\Seo\Values;
+
+class Value
+{
+    /**
+     * Value object original data
+     *
+     * @var mixed|null
+     */
+    protected $originalData;
+
+    /**
+     * Value object data
+     *
+     * @var mixed|null
+     */
+    protected $data;
+
+    /**
+     * Construtor.
+     *
+     * @param mixed|null $data
+     */
+    public function __construct($data = null)
+    {
+        $this->originalData = $data;
+    }
+
+    /**
+     * Get value data.
+     *
+     * @return mixed|null
+     */
+    public function data()
+    {
+        if ($data = $this->data) {
+            return $data;
+        }
+
+        return $this->originalData;
+    }
+}
