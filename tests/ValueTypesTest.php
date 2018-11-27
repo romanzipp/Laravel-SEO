9871b91484b405e9087124b829138deef22f4ad5
Roman Zipp <ich@ich.wtf>
2018-11-27 17:57:12 +0100

    Add struct attribute value types test

--- tests/ValueTypesTest.php
+++ tests/ValueTypesTest.php
@@ -4,6 +4,7 @@ namespace romanzipp\Seo\Test;
 
 use romanzipp\Seo\Facades\Seo;
 use romanzipp\Seo\Helpers\Hook;
+use romanzipp\Seo\Structs\Meta;
 use romanzipp\Seo\Structs\Title;
 use romanzipp\Seo\Test\TestCase;
 
@@ -15,9 +16,9 @@ class ValueTypesTest extends TestCase
             Title::make()->body(null)
         );
 
-        $title = seo()->getStructs()[0];
+        $struct = seo()->getStructs()[0];
 
-        $this->assertNull($title->getBody()->data());
+        $this->assertNull($struct->getBody()->data());
     }
 
     public function testStructBodyEmptyStringValueType()
@@ -26,9 +27,35 @@ class ValueTypesTest extends TestCase
             Title::make()->body('')
         );
 
-        $title = seo()->getStructs()[0];
+        $struct = seo()->getStructs()[0];
 
-        $this->assertTrue(is_string($title->getBody()->data()));
+        $this->assertTrue(is_string($struct->getBody()->data()));
+    }
+
+    public function testStructAttributeNullValueType()
+    {
+        seo()->add(
+            Meta::make()->attr('name', null)
+        );
+
+        $struct = seo()->getStructs()[0];
+
+        $attribute = $struct->getAttributes()['name'];
+
+        $this->assertNull($attribute->data());
+    }
+
+    public function testStructAttributeEmptyStringValueType()
+    {
+        seo()->add(
+            Meta::make()->attr('name', '')
+        );
+
+        $struct = seo()->getStructs()[0];
+
+        $attribute = $struct->getAttributes()['name'];
+
+        $this->assertTrue(is_string($attribute->data()));
     }
 
     public function testHookCallbackBodyType()
