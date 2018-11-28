c4a049020499ac944e80880b68426af9e3f996bf
Roman Zipp <ich@ich.wtf>
2018-11-28 03:39:27 +0100

    Add config shorthand index

--- config/seo.php
+++ config/seo.php
@@ -1,3 +1,36 @@
 <?php
 
-return [];
+return [
+
+    'shorthand' => [
+
+        /**
+         * Decide, which tags should be applied when using the
+         * Seo Service shorthand methods like
+         *
+         * seo()->title(...)
+         * seo()->description(...)
+         */
+
+        'title'       => [
+
+            // <title>...</title>
+            'tag'       => true,
+
+            // <meta property="og:title" content="..." />
+            'opengraph' => true,
+
+            // <meta name="twitter:title" content="..." />
+            'twitter'   => true,
+        ],
+
+        'description' => [
+
+            // <meta property="og:description" content="..." />
+            'opengraph' => true,
+
+            // <meta name="twitter:description" content="..." />
+            'twitter'   => true,
+        ],
+    ],
+];
