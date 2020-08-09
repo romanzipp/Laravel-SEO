aa645c406ee995ae38d66ac1a1c7c73b02fbc30d
Roman Zipp <ich@ich.wtf>
2020-08-09 10:55:13 +0200

    Add seo facade macroable method phpdoc

--- src/Facades/Seo.php
+++ src/Facades/Seo.php
@@ -5,6 +5,11 @@ namespace romanzipp\Seo\Facades;
 use Illuminate\Support\Facades\Facade;
 use romanzipp\Seo\Services\SeoService;
 
+/**
+ * @method static void macro($name, $macro)
+ * @method static void mixin($mixin, $replace = true)
+ * @method static bool hasMacro($name)
+ */
 class Seo extends Facade
 {
     protected static function getFacadeAccessor()
