d3912a1e88d38eb9bdb715edf065560815a93097
Peter Thaleikis <peter.thaleikis@gmail.com>
2020-10-16 14:41:35 +0400

    Adding "Article" namespace

---
+++ src/Structs/Meta/Article.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace romanzipp\Seo\Structs\Meta;
+
+use romanzipp\Seo\Structs\Meta;
+use romanzipp\Seo\Structs\Struct;
+
+/**
+ * @see https://github.com/joshbuchea/HEAD#facebook-open-graph
+ */
+class Article extends Meta
+{
+    protected $unique = true;
+
+    protected $uniqueAttributes = ['property'];
+
+    public function property($value = null, bool $escape = true): Struct
+    {
+        $this->addAttribute('property', 'article:' . $value, $escape);
+
+        return $this;
+    }
+}
